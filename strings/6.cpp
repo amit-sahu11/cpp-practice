1bc39bdbfeb2ec8eed6ccfb2d13dc129d8db95fb
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-02-28 18:22:39 +0530

    learning about strings

---
+++ strings/6.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str = "amit kumar sahu";  // string are dynamic means it can be changed at runtime
+    cout<<str<<endl;
+    str =  "hello amit";
+    cout<<str;
+}
