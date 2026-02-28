4b98176b09037c254dfd34a4e67c1e316b09f46e
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-02-28 21:59:48 +0530

    new changes

--- strings/6.cpp
+++ strings/6.cpp
@@ -5,5 +5,8 @@ int main(){
     string str = "amit kumar sahu";  // string are dynamic means it can be changed at runtime
     cout<<str<<endl;
     str =  "hello amit";
-    cout<<str;
+    cout<<str<<endl;
+    string str1 = "new word";
+    string str2 = str + str1;
+    cout<<str2;
 }
