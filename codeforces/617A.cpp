7197c46b985983600008eaa25187560b393560b3
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-03-05 23:42:45 +0530

    new changes

---
+++ codeforces/617A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    float d = n/4;
+    int e = (int)d;
+    if(d>e){
+        int e = e+1;
+    }
+    cout<<e;
+}
---
+++ strings/new.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s = "  hello world  ";
+    string ans = "";
+    reverse(s.begin(),s.end());
+    for(int i=0;i<s.length();i++){
+
+    }
+}
