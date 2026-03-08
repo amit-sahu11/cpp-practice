39bb3a259179fb29d017a24afd5aec4f18ea494c
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-03-08 19:49:49 +0530

    new

--- 2D-Vector/4.cpp
+++ 2D-Vector/4.cpp
@@ -1,45 +1,54 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    vector<vector<int>> matrix = {{1,2,3},{4,5,6},{7,8,9}};
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     vector<int> new_matrix;
 
     int m = matrix.size();
     int n = matrix[0].size();
 
-    int sR = 0, eR = m-1;
-    int sC = 0, eC = n-1;
+    int sR = 0, eR = m - 1;
+    int sC = 0, eC = n - 1;
 
-    while(sR <= eR && sC <= eC){
-        for(int i = sC; i <= eC; i++){
+    while (sR <= eR && sC <= eC)
+    {
+        for (int i = sC; i <= eC; i++)
+        {
             new_matrix.push_back(matrix[sR][i]);
         }
         sR++;
 
-        for(int i = sR; i <= eR; i++){
+        for (int i = sR; i <= eR; i++)
+        {
             new_matrix.push_back(matrix[i][eC]);
         }
         eC--;
 
-        if(sR <= eR){
-            for(int i = eC; i >= sC; i--){
+        if (sR <= eR)
+        {
+            for (int i = eC; i >= sC; i--)
+            {
                 new_matrix.push_back(matrix[eR][i]);
             }
             eR--;
         }
 
-        if(sC <= eC){
-            for(int i = eR; i >= sR; i--){
+        if (sC <= eC)
+        {
+            for (int i = eR; i >= sR; i--)
+            {
                 new_matrix.push_back(matrix[i][sC]);
             }
             sC++;
         }
     }
 
-    for(int x : new_matrix){
+    for (int x : new_matrix)
+    {
         cout << x << " ";
     }
 }
