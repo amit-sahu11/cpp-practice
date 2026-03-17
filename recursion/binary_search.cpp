ceb6085a2497b4442c8436893ce5e72527dcab03
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-03-17 20:26:30 +0530

    recursion

--- recursion/binary_search.cpp
+++ recursion/binary_search.cpp
@@ -2,10 +2,21 @@
 #include<vector>
 using namespace std;
 
-int binary_search(int arr, int target,int st,int end){
-    
+int binary_search(vector<int> arr, int target,int st,int end){
+    if(st<=end){
+        int mid = st+(end-st)/2;
+        if(arr[mid]==target) return mid;
+        else if(arr[mid]<=target){
+            return binary_search(arr,target,mid+1,end);
+        }
+        else{
+            return binary_search(arr,target,st,mid-1);
+        }
+    }
+    return -1;
 }
 int main(){
     vector<int> arr = {1,2,3,4,5,6};
-    int st = 0,end=(arr.size())-1;
+    int index = binary_search(arr,3,0,arr.size()-1);
+    cout<<index;
 }
