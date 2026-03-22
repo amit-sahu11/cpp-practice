1b27a2e4b8f78cdd132d396816573a53e6e49136
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-03-22 11:12:19 +0530

    recursion

--- recursion/factorial.cpp
+++ recursion/factorial.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int rec(int n)
+int factorial(int n)
 {
     if(n==0 || n==1) return 1;
-    return n*rec(n-1);
+    return n*factorial(n-1);
 }
 int main()
 {
     int n=5;
-    cout<<rec(n);
+    cout<<factorial(n);
 }
