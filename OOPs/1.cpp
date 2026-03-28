42ae413aa6648ab1a0ee879a44c97c243b483e4a
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-03-28 15:56:34 +0530

    oops

--- OOPs/1.cpp
+++ OOPs/1.cpp
@@ -3,18 +3,39 @@
 using namespace std;
 
 class Teacher{
+private:
+    double salary;
+
+public:
     //Properties
     string name;
     string dept;
     string subject;
-    double salary;
 
     //Methods / Member function
     void changeDept(string newDept){
         dept = newDept;
     }
+
+    // setter 
+    void setSalaray(double s){
+        salary = s;
+    }
+
+    //getter
+    double getSalary(){
+        return salary;
+    }
 };
 
 int main(){
     Teacher t1;
+    t1.name = "amit";
+    t1.subject = "c++";
+    t1.dept = "computer science";
+    t1.setSalaray(25000);
+
+    cout<<t1.name<<endl;
+    cout<<t1.getSalary()<<endl;
+    return 0;
 }
