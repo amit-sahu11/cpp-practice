ec2c44a3d1945301c5e7c1bf518ca26dbd0b24f6
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-03-28 23:16:32 +0530

    constructor

---
+++ OOPs/constructor.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Teacher{
+private:
+    double salary;
+
+public:
+    Teacher(){
+        cout<<"Hii, this is constructor call"<<endl;
+    }
+    //Properties
+    string name;
+    string dept;
+    string subject;
+
+    //Methods / Member function
+    void changeDept(string newDept){
+        dept = newDept;
+    }
+
+    // setter 
+    void setSalaray(double s){
+        salary = s;
+    }
+
+    //getter
+    double getSalary(){
+        return salary;
+    }
+};
+
+int main(){
+    Teacher t1; //constructor call
+    Teacher t2; 
+    t1.name = "amit";
+    t1.subject = "c++";
+    t1.dept = "computer science";
+    t1.setSalaray(25000);
+
+    cout<<t1.name<<endl;
+    cout<<t1.getSalary()<<endl;
+    return 0;
+}
