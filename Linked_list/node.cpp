f9741647c4dd65d3d9885cf70d0905c25b1817b7
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-04-06 21:10:53 +0530

    node

---
+++ Linked_list/node.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Node{
+    public:
+        int value;
+        Node* next;
+
+        Node(int v){
+            value = v;
+            next = NULL;
+        }
+};
+
+int main(){
+    Node* node1 = new Node(1);
+    Node* node2 = new Node(2);
+    node1->next = node2;
+}
