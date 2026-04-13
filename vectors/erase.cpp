dd8978b4ab753dc590c1c27af71f7fc485c0de6c
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-04-13 21:01:37 +0530

    erase

--- vectors/erase.cpp
+++ vectors/erase.cpp
@@ -4,6 +4,7 @@
 #include<vector>
 using namespace std;
 int main(){
+    
     vector<int> vec = {1,2,3,4,5,6,7,8,9};
     vector<int> vec1 = {1,2,3,4,5,6,7,8,9};
     cout<<*vec.erase(vec.begin());
