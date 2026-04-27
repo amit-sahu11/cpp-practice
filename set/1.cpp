c8cad8d6cf24160d37ffecf5ae0b210b8ac4e5f4
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-04-27 23:48:18 +0530

    set

--- set/1.cpp
+++ set/1.cpp
@@ -10,6 +10,7 @@ int main(){
     s.emplace(7);
     cout<<s.size()<<endl;
     cout<<*(s.lower_bound(4))<<endl;
+    
 
     // lower bound give the exact value 4 and if 4 is not available it give just one above valur means 5 and if it is also not available it will give s.end() value means koi bhi valid anser exist nhi krta and when we will print the defrence value it will give the output as 0
 
