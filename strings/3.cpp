7d762b203aad1b1f61f3e93793049ac745a78598
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-05-19 23:36:02 +0530

    strings

--- strings/3.cpp
+++ strings/3.cpp
@@ -7,7 +7,6 @@ int main(){
     // cout<<ch<<endl;
     // if we enter the input "amit sahu" it will only print hello because it c++ it ignores the value written after space
     // so to overcome this problem we use getline()
-
     // cin.getline(str,len,delim?)
     char c[100];
     // cin.getline(c,100);
