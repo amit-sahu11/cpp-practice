3026a7a3775543386fe330fc9a80aa3e5047717a
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-06-04 20:04:46 +0530

    fox and snake

--- codeforces/fox_and_snake.cpp
+++ codeforces/fox_and_snake.cpp
@@ -7,21 +7,21 @@ int main(){
 
     for(int i = 0; i < n; i++){
         if(i % 2 == 0){
-            // full row
+
             for(int j = 0; j < m; j++){
                 cout << "#";
             }
         }
         else{
             if((i / 2) % 2 == 0){
-                // snake on right
+              
                 for(int j = 0; j < m - 1; j++){
                     cout << ".";
                 }
                 cout << "#";
             }
             else{
-                // snake on left
+               
                 cout << "#";
                 for(int j = 0; j < m - 1; j++){
                     cout << ".";
