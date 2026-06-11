d0f7ef6c0f883a167bcf4edb9ad6a7d16b654f1b
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-06-11 23:29:15 +0530

    arrays

--- arrays/2.cpp
+++ arrays/2.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(){
     int smallest = INT_MAX;
     int n = 6;
-    
     int arr[6]={4,5,6,7,8,9};
+    
     for(int i = 0;i<n;i++){
         smallest = min(arr[i],smallest);
     }
