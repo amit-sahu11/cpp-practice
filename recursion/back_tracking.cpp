899a994070deafc26de50033c8ef1f0e61bca11b
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-06-13 23:38:44 +0530

    recursion

--- recursion/back_tracking.cpp
+++ recursion/back_tracking.cpp
@@ -11,6 +11,7 @@ void printSubsets(vector<int> &arr,vector<int> &ans,int i){
         cout<<endl;
         return;
     }
+    
     // include
     ans.push_back(arr[i]);
     printSubsets(arr,ans,i+1);
