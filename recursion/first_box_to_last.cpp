2b9839e4d13be245105e5c6cc2cde79933d340d6
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-06-23 23:25:51 +0530

    recursion

--- recursion/first_box_to_last.cpp
+++ recursion/first_box_to_last.cpp
@@ -7,6 +7,7 @@ void helper(vector<vector<int>>&mat,int r,int c,string path,vector<string> &ans)
     if(r<0 || c<0 || r>=n || c>=n || mat[r][c]==0 || mat[r][c] == -1){
         return;
     }
+    
     if(r == n-1 && c==n-1){
         ans.push_back(path);
         return;
