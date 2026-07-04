d4974c73d7d44672122e5c482fd1fe96f7f5c604
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-07-04 23:23:48 +0530

    vectors

--- vectors/5.cpp
+++ vectors/5.cpp
@@ -8,6 +8,7 @@ vector<int> pairsum(vector<int> nums,int target){
     int n = nums.size();
     for(int i=0;i<n;i++){
         for(int j = i+1;j<n;j++){
+            
             if(nums[i]+nums[j]==target){
                 ans.push_back(i);
                 ans.push_back(j);
