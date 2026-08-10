3f0c67b1e0b211c1f7a7f1afe9b7f1f05eb537b1
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-08-10 22:47:04 +0530

    pair

---
+++ pair/4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+int main(){
+    pair<int, string> p1 = {1, "Apple"};
+    cout << p1.first << " " << p1.second << endl;
+    return 0;
+}
