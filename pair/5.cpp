0eaaa60a1c2aba63c4e4bde0475fb5be94a33c0c
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-08-10 22:47:37 +0530

    pair

---
+++ pair/5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+int main(){
+    pair<int, string> p3;
+    p3.first = 3;
+    p3.second = "Cherry";
+    cout << p3.first << " " << p3.second << endl;
+    return 0;
+}
