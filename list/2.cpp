dbe03f9ab7db4b7a5e4e8f476534dd916a63e89b
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-08-18 23:08:53 +0530

    list

---
+++ list/2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+    list<int> l;
+
+    l.push_back(10);
+    l.push_back(20);
+    l.push_front(5);
+
+    for (int x : l) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
