389062629225e96e0bd5ceceb19a7eb9a94b4388
amit kumr sahu <sahuamitsahu87@gmail.com>
2026-08-21 23:46:49 +0530

    pointers

---
+++ pointer/5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void swapNum(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a = 10, b = 20;
+
+    swapNum(&a, &b);
+
+    cout << a << " " << b;
+
+    return 0;
+}
